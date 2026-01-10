37bbc7093bd7ba03cc4977b2903c52395c673c59
selman alshemali <justin22wi@gmail.com>
2026-01-10 12:50:05 +0100

    app psr7 for request and response

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,11 +2,35 @@
 
 declare(strict_types=1);
 
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\ServerRequest;
+use GuzzleHttp\Psr7\Utils;
+use HttpSoft\Emitter\SapiEmitter;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
 // error reporting
 ini_set('display_errors', '1');
 
-// get page
-$page = $_GET['page'];
+$requst = ServerRequest::fromGlobals();
 
-// require page
+$page = $requst->getQueryParams()['page'] ?? 'home';
+
+$route =
+    ob_start();
+
 require dirname(__DIR__) . '/' . $page . '.php';
+
+$content = ob_get_clean();
+
+$stream = Utils::streamFor($content);
+
+$response = new Response;
+
+$response = $response->withHeader('Content-Type', 'text/html')
+    ->withStatus(418)
+    ->withBody($stream);
+
+$emitter = new SapiEmitter;
+
+$emitter->emit($response);
